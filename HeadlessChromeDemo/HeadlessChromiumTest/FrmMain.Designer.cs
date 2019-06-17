92ed463ddc53a1ab59b07a2c50aea16a3447979b
李志勇 <lizhiyong@juxinli.com>
2019-06-17 16:38:34 +0800

    代码优化

--- HeadlessChromeDemo/HeadlessChromiumTest/FrmMain.Designer.cs
+++ HeadlessChromeDemo/HeadlessChromiumTest/FrmMain.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace HeadlessChromiumTest
 {
-    partial class Form1
+    partial class FrmMain
     {
         /// <summary>
         /// 必需的设计器变量。
@@ -28,34 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btn_chromiumTest = new System.Windows.Forms.Button();
+            this.rTxt_log = new System.Windows.Forms.RichTextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // button1
+            // btn_chromiumTest
             // 
-            this.button1.Location = new System.Drawing.Point(16, 20);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(173, 53);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "无头浏览器测试";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btn_chromiumTest.Location = new System.Drawing.Point(16, 20);
+            this.btn_chromiumTest.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btn_chromiumTest.Name = "btn_chromiumTest";
+            this.btn_chromiumTest.Size = new System.Drawing.Size(173, 53);
+            this.btn_chromiumTest.TabIndex = 0;
+            this.btn_chromiumTest.Text = "无头浏览器测试";
+            this.btn_chromiumTest.UseVisualStyleBackColor = true;
+            this.btn_chromiumTest.Click += new System.EventHandler(this.btn_chromiumTest_Click);
             // 
-            // richTextBox1
+            // rTxt_log
             // 
-            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.rTxt_log.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.richTextBox1.Location = new System.Drawing.Point(16, 128);
-            this.richTextBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.ReadOnly = true;
-            this.richTextBox1.Size = new System.Drawing.Size(406, 320);
-            this.richTextBox1.TabIndex = 1;
-            this.richTextBox1.Text = "";
+            this.rTxt_log.Location = new System.Drawing.Point(16, 128);
+            this.rTxt_log.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.rTxt_log.Name = "rTxt_log";
+            this.rTxt_log.ReadOnly = true;
+            this.rTxt_log.Size = new System.Drawing.Size(406, 320);
+            this.rTxt_log.TabIndex = 1;
+            this.rTxt_log.Text = "";
             // 
             // label1
             // 
@@ -69,21 +69,21 @@
             this.label1.TabIndex = 2;
             this.label1.Text = "Log：";
             // 
-            // Form1
+            // FrmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(440, 471);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.richTextBox1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.rTxt_log);
+            this.Controls.Add(this.btn_chromiumTest);
             this.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.MaximizeBox = false;
-            this.Name = "Form1";
+            this.Name = "FrmMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Form1";
-            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Text = "Chromium 测试";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FrmMain_FormClosing);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -91,8 +91,8 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btn_chromiumTest;
+        private System.Windows.Forms.RichTextBox rTxt_log;
         private System.Windows.Forms.Label label1;
     }
 }
--- HeadlessChromeDemo/HeadlessChromiumTest/FrmMain.cs
+++ HeadlessChromeDemo/HeadlessChromiumTest/FrmMain.cs
@@ -14,9 +14,9 @@ using System.Windows.Forms;
 
 namespace HeadlessChromiumTest
 {
-    public partial class Form1 : Form
+    public partial class FrmMain : Form
     {
-        public Form1()
+        public FrmMain()
         {
             InitializeComponent();
         }
@@ -25,7 +25,7 @@ namespace HeadlessChromiumTest
         /// <summary>
         /// 测试 URL
         /// </summary>
-        private const string _url = "https://hanyu.baidu.com/s?wd=%E8%85%BE&ptype=zici";
+        private const string _testUrl = "https://hanyu.baidu.com/s?wd=%E8%85%BE&ptype=zici";
 
         /// <summary>
         /// 程序根目录
@@ -33,20 +33,20 @@ namespace HeadlessChromiumTest
         private static readonly string _programRoot = AppDomain.CurrentDomain.BaseDirectory;
 
 
+
         /// <summary>
         /// 测试无头浏览器
         /// </summary>
         /// <param name="isDisplay">是否显示界面</param>
-        /// <param name="executablePath">Chromium 或 Chrome 可执行文件的路径</param>
         /// <returns></returns>
-        private async Task<string> TestHeadlessChromiumAsync(bool isDisplay = true, string executablePath = null)
+        private async Task<string> TestHeadlessChromiumAsync(bool isDisplay = true)
         {
             this.Invoke(new Action(() =>
             {
-                this.richTextBox1.AppendText("正在启动无头浏览器... \n");
+                this.rTxt_log.AppendText("正在启动无头浏览器... \n");
             }));
 
-            #region 兼容 Win7 及以下系统
+            #region 兼容 Win7 及以下系统 （包含 Win7）
 
             LaunchOptions launchOptions;
             if (OSHelper.IsWin7Under())
@@ -74,40 +74,34 @@ namespace HeadlessChromiumTest
             else
             {
                 launchOptions = new LaunchOptions();
-                if (executablePath != null)
-                {
-                    launchOptions.ExecutablePath = executablePath;
-                }
             }
             launchOptions.Headless = !isDisplay; // Headless true 是无头模式；false 是开发者模式，有界面
 
             #endregion
 
+            string htmlContent = string.Empty;
+
             // 启动 Chromium 浏览器
-            Browser browser = await Puppeteer.LaunchAsync(launchOptions);
-
-            this.Invoke(new Action(() =>
+            using (Browser browser = await Puppeteer.LaunchAsync(launchOptions))
             {
-                this.richTextBox1.AppendText("无头浏览器已启动。 \n");
-            }));
+                this.Invoke(new Action(() =>
+                {
+                    this.rTxt_log.AppendText("无头浏览器已启动。 \n");
+                }));
 
-            // 新建一个标签页
-            Page page = await browser.NewPageAsync();
+                // 新建一个标签页
+                using (Page page = await browser.NewPageAsync())
+                {
+                    // 导航到 url 页
+                    await page.GoToAsync(_testUrl);
 
-            // 导航到 url 页
-            await page.GoToAsync(_url);
+                    // 获取并返回页面的HTML内容
+                    htmlContent = await page.GetContentAsync();
 
-            // 获取并返回页面的HTML内容
-            string htmlContent = await page.GetContentAsync();
-
-
-            // 界面停留，给开发人员看
-            await Task.Delay(14 * 1000);
-
-
-            await page.CloseAsync();    // 关闭标签页
-
-            await browser.CloseAsync(); // 关闭浏览器
+                    // 界面停留，给开发人员看
+                    await Task.Delay(14 * 1000);
+                }
+            }
 
             return htmlContent;
         }
@@ -137,32 +131,31 @@ namespace HeadlessChromiumTest
 
 
 
-        private void button1_Click(object sender, EventArgs e)
+        private void btn_chromiumTest_Click(object sender, EventArgs e)
         {
-            this.button1.Enabled = false;
-            this.richTextBox1.Clear();
+            this.btn_chromiumTest.Enabled = false;
+            this.rTxt_log.Clear();
 
             Task.Run(async () =>
             {
                 #region 无头浏览器下载更新
 
                 BrowserFetcher browserFetcher = Puppeteer.CreateBrowserFetcher(new BrowserFetcherOptions());
-                // 533271 、 BrowserFetcher.DefaultRevision
                 RevisionInfo revisionInfo = browserFetcher.RevisionInfo(BrowserFetcher.DefaultRevision);
 
                 if (revisionInfo.Downloaded && revisionInfo.Local)
                 {
                     this.Invoke(new Action(() =>
                     {
-                        this.richTextBox1.AppendText("无头浏览器检查完成，无需更新！ \n");
+                        this.rTxt_log.AppendText("无头浏览器检查完成，无需更新！ \n");
                     }));
                 }
                 else
                 {
                     this.Invoke(new Action(() =>
                     {
-                        this.richTextBox1.AppendText("无头浏览器检查完成，需要更新！ \n");
-                        this.richTextBox1.AppendText("正在检查更新... \n");
+                        this.rTxt_log.AppendText("无头浏览器检查完成，需要更新！ \n");
+                        this.rTxt_log.AppendText("正在检查更新... \n");
                     }));
 
                     // 检查 revisionInfo.Revision 这个版本的 Chromium 浏览器 是否 可用\可下载
@@ -171,15 +164,15 @@ namespace HeadlessChromiumTest
                     {
                         this.Invoke(new Action(() =>
                         {
-                            this.richTextBox1.AppendText($"程序检测出无头浏览器（默认版本 {revisionInfo.Revision}）无法更新！ \n");
-                            this.button1.Enabled = true;
+                            this.rTxt_log.AppendText($"程序检测出无头浏览器（默认版本 {revisionInfo.Revision}）无法更新！ \n");
+                            this.btn_chromiumTest.Enabled = true;
                         }));
                         return;
                     }
 
                     this.Invoke(new Action(() =>
                     {
-                        this.richTextBox1.AppendText("正在更新无头浏览器... \n");
+                        this.rTxt_log.AppendText("正在更新无头浏览器... \n");
                     }));
 
                     // 下载 revisionInfo.Revision 这个版本的无头浏览器；可能需要等待一些时间
@@ -187,7 +180,7 @@ namespace HeadlessChromiumTest
 
                     this.Invoke(new Action(() =>
                     {
-                        this.richTextBox1.AppendText("无头浏览器更新完成。 \n");
+                        this.rTxt_log.AppendText("无头浏览器更新完成。 \n");
                     }));
                 }
 
@@ -195,15 +188,15 @@ namespace HeadlessChromiumTest
 
                 this.Invoke(new Action(() =>
                 {
-                    this.richTextBox1.AppendText("开始测试无头浏览器... \n");
+                    this.rTxt_log.AppendText("开始测试无头浏览器... \n");
                 }));
 
-                string htmlContent = await TestHeadlessChromiumAsync(true, revisionInfo.ExecutablePath);
+                string htmlContent = await TestHeadlessChromiumAsync();
 
                 this.Invoke(new Action(() =>
                 {
-                    this.richTextBox1.AppendText("无头浏览器测试完成。 \n");
-                    this.button1.Enabled = true;
+                    this.rTxt_log.AppendText("无头浏览器测试完成。 \n");
+                    this.btn_chromiumTest.Enabled = true;
                 }));
 
                 WriteCreate(_programRoot + $"SaveHtmlPage\\{DateTime.Now.ToString("yyyy-MM-dd_HH.mm.ss")}.html", htmlContent);
@@ -211,7 +204,7 @@ namespace HeadlessChromiumTest
         }
 
 
-        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        private void FrmMain_FormClosing(object sender, FormClosingEventArgs e)
         {
             System.Environment.Exit(0);
         }
