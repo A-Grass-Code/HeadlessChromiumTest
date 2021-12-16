f4d1ff60aef5c255bb97b1146e769db40357d798
A-Grass-Code <50985469+A-Grass-Code@users.noreply.github.com>
2021-12-16 10:16:35 +0800

    窗体中加入 “本地 Chrome 浏览器测试” 按钮 - 1

--- HeadlessChromeDemo/HeadlessChromium.Test/FrmMain.Designer.cs
+++ HeadlessChromeDemo/HeadlessChromium.Test/FrmMain.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace HeadlessChromium.Test
+namespace HeadlessChromium.Test
 {
     partial class FrmMain
     {
@@ -32,14 +32,15 @@
             this.rTxt_log = new System.Windows.Forms.RichTextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.btn_chromiumTest_Encapsulated = new System.Windows.Forms.Button();
+            this.btn_nativeChromeTest = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btn_chromiumTest
             // 
-            this.btn_chromiumTest.Location = new System.Drawing.Point(16, 20);
+            this.btn_chromiumTest.Location = new System.Drawing.Point(13, 14);
             this.btn_chromiumTest.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btn_chromiumTest.Name = "btn_chromiumTest";
-            this.btn_chromiumTest.Size = new System.Drawing.Size(142, 53);
+            this.btn_chromiumTest.Size = new System.Drawing.Size(142, 43);
             this.btn_chromiumTest.TabIndex = 0;
             this.btn_chromiumTest.Text = "无头浏览器测试";
             this.btn_chromiumTest.UseVisualStyleBackColor = true;
@@ -50,11 +51,11 @@
             this.rTxt_log.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.rTxt_log.Location = new System.Drawing.Point(16, 128);
+            this.rTxt_log.Location = new System.Drawing.Point(13, 103);
             this.rTxt_log.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.rTxt_log.Name = "rTxt_log";
             this.rTxt_log.ReadOnly = true;
-            this.rTxt_log.Size = new System.Drawing.Size(406, 320);
+            this.rTxt_log.Size = new System.Drawing.Size(558, 295);
             this.rTxt_log.TabIndex = 1;
             this.rTxt_log.Text = "";
             // 
@@ -63,7 +64,7 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
-            this.label1.Location = new System.Drawing.Point(13, 104);
+            this.label1.Location = new System.Drawing.Point(13, 79);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(49, 19);
@@ -72,20 +73,32 @@
             // 
             // btn_chromiumTest_Encapsulated
             // 
-            this.btn_chromiumTest_Encapsulated.Location = new System.Drawing.Point(214, 20);
+            this.btn_chromiumTest_Encapsulated.Location = new System.Drawing.Point(163, 14);
             this.btn_chromiumTest_Encapsulated.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btn_chromiumTest_Encapsulated.Name = "btn_chromiumTest_Encapsulated";
-            this.btn_chromiumTest_Encapsulated.Size = new System.Drawing.Size(208, 53);
+            this.btn_chromiumTest_Encapsulated.Size = new System.Drawing.Size(208, 43);
             this.btn_chromiumTest_Encapsulated.TabIndex = 3;
             this.btn_chromiumTest_Encapsulated.Text = "无头浏览器测试（封装版）";
             this.btn_chromiumTest_Encapsulated.UseVisualStyleBackColor = true;
             this.btn_chromiumTest_Encapsulated.Click += new System.EventHandler(this.btn_chromiumTest_Encapsulated_Click);
             // 
+            // btn_nativeChromeTest
+            // 
+            this.btn_nativeChromeTest.Location = new System.Drawing.Point(379, 14);
+            this.btn_nativeChromeTest.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btn_nativeChromeTest.Name = "btn_nativeChromeTest";
+            this.btn_nativeChromeTest.Size = new System.Drawing.Size(192, 43);
+            this.btn_nativeChromeTest.TabIndex = 4;
+            this.btn_nativeChromeTest.Text = "本地 Chrome 浏览器测试";
+            this.btn_nativeChromeTest.UseVisualStyleBackColor = true;
+            this.btn_nativeChromeTest.Click += new System.EventHandler(this.btn_nativeChromeTest_Click);
+            // 
             // FrmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(440, 471);
+            this.ClientSize = new System.Drawing.Size(584, 412);
+            this.Controls.Add(this.btn_nativeChromeTest);
             this.Controls.Add(this.btn_chromiumTest_Encapsulated);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.rTxt_log);
@@ -108,6 +121,7 @@
         private System.Windows.Forms.RichTextBox rTxt_log;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btn_chromiumTest_Encapsulated;
+        private System.Windows.Forms.Button btn_nativeChromeTest;
     }
 }
 
